684b93cabe96fc6aaca1ad0c0adef937f3f21855
Alexandra Cardoso <agpco2@iscte-iul.pt>
2025-12-19 17:51:51 +0000

    CORRIGIDO EM PRINCIPIO

--- bd-parte-2/src/bd_estagios.php
+++ bd-parte-2/src/bd_estagios.php
@@ -53,7 +53,7 @@ class Estagio extends Estagios {
 	}
  
 	function novoEstagio($empresa_id, $estabelecimento_id, $aluno_id, $formador_id, $data_inicio) {
-		$sql = "INSERT INTO estagio (estabelecimento_empresa_id, estabelecimento_id, aluno_id, formador_id, data_inicio) VALUES ($empresa_id, $estabelecimento_id, $aluno_id, $formador_id, $data_inicio)";
+		$sql = "INSERT INTO estagio (estabelecimento_empresa_id, estabelecimento_id, aluno_id, formador_id, data_inicio) VALUES ($empresa_id, $estabelecimento_id, $aluno_id, $formador_id, '$data_inicio')";
 		$this->db_estagios->executarSQL($sql);
 	}
 

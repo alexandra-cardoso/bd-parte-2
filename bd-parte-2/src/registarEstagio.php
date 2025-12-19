7787cd0d38efbde73fa6577c9e09d276ed5523f9
Alexandra Cardoso <agpco2@iscte-iul.pt>
2025-12-19 18:10:43 +0000

    registarEstagio deve funcionar

--- bd-parte-2/src/registarEstagio.php
+++ bd-parte-2/src/registarEstagio.php
@@ -6,11 +6,11 @@
     if(isset($_POST['salvar'])) {
         $id_estab = $_POST['estab_id'];
         $id_emp = $_POST['emp_id'];
-        $data_inicio = $_POST['data_ini'];
+        $data_inicio = $_POST['data_inicial'];
         $aluno_id = $_POST['aluno_id'];
         $formador_id = $_POST['formador_id'];
 
-        $estagios->novoEstagio($id_emp, $id_estab, $data_ini, $aluno_id, $formador_id);
+        $estagios->novoEstagio($id_emp, $id_estab, $aluno_id, $formador_id, $data_inicial);
     }
 ?>
 <html>
@@ -26,7 +26,7 @@
             <form action = "registarEstagio.php" method=post>
                 ID do Estabelecimento: <input type=text name = estab_id><br>
                 ID da Empresa: <input type=text name = emp_id><br>
-                Data de Início do Estágio: <input type=text name = data_ini><br>
+                Data de Início do Estágio: <input type=text name = data_inicial><br>
                 ID do Aluno: <input type=text name = aluno_id><br>
                 ID do Formador: <input type=text name = formador_id><br>
                 <input type=submit name = salvar value = "Guardar Estágio">
